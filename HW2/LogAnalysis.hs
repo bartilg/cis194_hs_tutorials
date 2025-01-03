6c52ac55ebe6d51e3c627adb5813f03ce3307208
bartilg <35232099+bartilg@users.noreply.github.com>
2025-01-03 14:00:04 -0800

    solved hw2 question 1

--- HW2/LogAnalysis.hs
+++ HW2/LogAnalysis.hs
@@ -1,6 +1,17 @@
-{-# OPTIONS_GHC -WALL #-}
+{-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 import Log
 
 parseMessage :: String -> LogMessage
-parseMessage x = 
+parseMessage x = case words x of
+    "I":n:msg -> LogMessage Info (read n) (unwords msg)
+    "W":n:msg -> LogMessage Warning (read n) (unwords msg)
+    "E":n:n2:msg -> LogMessage (Error (read n)) (read n2) (unwords msg)
+    _ -> Unknown x
+    
+parse:: String -> [LogMessage]
+parse s = map parseMessage (lines s)
+
+main :: IO [LogMessage]
+main = do 
+    testParse parse 10 "error.log"
