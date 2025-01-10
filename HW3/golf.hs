be64af80deefd9a29e2f64a8242c8ffc483dd02d
bartilg <35232099+bartilg@users.noreply.github.com>
2025-01-10 09:48:00 -0800

    solve hw3 exercise 2

--- HW3/golf.hs
+++ HW3/golf.hs
@@ -1,12 +1,34 @@
 module Golf where
+  
+range :: [a] -> [Int]
+range n = [1 .. (length n)]
 
-safeTail :: [a] -> Maybe a
-safeTail [] = Nothing
-safeTail _:xs = Just xs 
+skipper :: [a] -> Int -> [a]
+skipper [] _ = []
+skipper a n = case drop (n-1) a of
+  (x:xs) -> x : skipper xs n
+  [] -> []
 
 skips :: [a] -> [[a]]
-skips [] = []
-skips a:xs = [a:xs] ++ skips (safeTail xs)
+skips a = map (\n -> skipper a n) (range a)
 
+midMax :: Integer -> Integer -> Integer -> Bool
+midMax x y z = x < y && y > z
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:rest) 
+  | midMax x y z = y : localMaxima(y:z:rest)
+  | otherwise = localMaxima(y:z:rest)
+localMaxima _ = []
+
+histogram :: [Integer] -> String
+
+
+main :: IO ()
 main = do 
-  skips "testing"
+  print (skips "ABCDEFG")
+  print (skips "ABCD")
+  print(localMaxima [2,9,5,6,1])
+  print(localMaxima [2,3,4,1,5])
+  print(localMaxima [1,2,3,4,5])
+  print(localMaxima [1,2])
