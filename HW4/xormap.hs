2ba94c3712d6900959099b7ecc508c296a33ccb7
Bart Ilg <bartilg1215@gmail.com>
2025-01-12 14:48:41 -0800

    Solved map question

--- HW4/xormap.hs
+++ HW4/xormap.hs
@@ -7,6 +7,13 @@ xorHelper True x
 xor :: [Bool] -> Bool
 xor = foldr xorHelper False
 
+mapHelper :: (a -> b) -> a -> [b] -> [b]
+mapHelper fun x acc = (fun x) : acc
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (mapHelper f) []
+
 main = do 
   print (xor [False, True, False])
   print (xor [False, True, False, False, True])
+  print (map' (*2) [1, 2, 3])
