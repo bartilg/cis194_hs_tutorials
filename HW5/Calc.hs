bccb639b4ee4d8d524d06cfbc9a098594ee7ca4c
Bart Ilg <bartilg1215@gmail.com>
2025-02-07 12:37:47 -0800

    Solved HW5 Exercise 4

--- HW5/Calc.hs
+++ HW5/Calc.hs
@@ -1,14 +1,17 @@
 import ExprT
 import Parser
 
+-- Part 1
 eval :: ExprT -> Integer
 eval (Lit n) = n
 eval (Add n n2) = (eval n) + (eval n2)
 eval (Mul n n2) = (eval n) * (eval n2)
 
+-- Part 2
 evalStr :: String -> Maybe Integer
 evalStr s = maybe Nothing (Just . eval) (parseExp Lit Add Mul s)
 
+-- Part 3
 class Expr a where
   lit :: Integer -> a
   add :: a -> a -> a
@@ -22,6 +25,44 @@ instance Expr ExprT where
 reify :: ExprT -> ExprT
 reify = id
 
+-- Part 4
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7   = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit x
+    | x <= 0 = False
+    | otherwise = True
+  add = (||)
+  mul = (&&)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
+
 main = do
   print (eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)))
   print (evalStr "(2+3)*4")
+  print (testInteger)
+  print (testBool)
+  print (testMM)
+  print (testSat)
