fbb0e0c0dbe85bd0ecd3dd86839a3773deea3000
Bart Ilg <bartilg1215@gmail.com>
2025-02-07 12:12:08 -0800

    Solved HW5 Exercise 2,3

--- HW5/Calc.hs
+++ HW5/Calc.hs
@@ -1,9 +1,27 @@
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit n) = n
 eval (Add n n2) = (eval n) + (eval n2)
 eval (Mul n n2) = (eval n) * (eval n2)
 
+evalStr :: String -> Maybe Integer
+evalStr s = maybe Nothing (Just . eval) (parseExp Lit Add Mul s)
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
 main = do
   print (eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)))
+  print (evalStr "(2+3)*4")
